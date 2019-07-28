0774f9e05dd8a9dcad41d19beb31741132d82160
github username <github email address>
2019-07-28 17:17:22 -0500

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,38 @@
 cards = [
   {phrase: "Lit", acronym: "If something is lit, it means it's super cool or on fire.
-Last night's party was lit.", hint: "COOL", img_url: "https://steamuserimages-a.akamaihd.net/ugc/609476610127244242/AA3757BEFCE719580024B3296AD112E0B33EEB78/", punishment: nil},
-{phrase: "Low key", acronym: "If someone or something is low key, it means it's being done under the radar or they don't want anyone to know.", hint: "Secret", img_url: "https://static.scientificamerican.com/sciam/cache/file/00A865C6-9F1F-4412-A9CA2FC329C15F36_source.jpg?w=590&h=800&08F4EA30-9038-4EB2-8EBF9195D8829B6C", punishment: nil},
-{phrase: "Spaghetti on the Wall", acronym: "try something to see what works.", hint: "If it sticks, cooked well", img_url: "http://static1.squarespace.com/static/5851ea8d15d5db7317b11176/585c87a28419c2107deb3596/58b5cd171b631bbb2f832ff8/1501089566642/1ffcc2e.jpg?format=1500w", punishment: nil},
-{phrase: "Salty", acronym: "Angry or bitter about something.", hint: "I don't know, you figure it out", img_url: "https://pbs.twimg.com/profile_images/835940654509342724/chujJdF__400x400.jpg", punishment: nil},
-{phrase: "Savage", acronym: "Someone who roasts people nonstop and doesn't care what others will say.", hint: "a** hole", img_url: "https://iruntheinternet.com/lulzdump/images/flip-off-cat-middle-finger-goes-mad-bites-it-rage-14344850449.jpg", punishment: nil},
-{phrase: "Ship", acronym: "Short for romantic relationship, sometimes used as a verb.", hint: "everyone wants to board on something", img_url: "https://i.redd.it/xsm9auf4tfq01.jpg", punishment: nil},
-{phrase: "Throw shade", acronym: "To insult or say something unkind about someone.", hint: "throw the guy in image", img_url: "https://cdn3-www.comingsoon.net/assets/uploads/2017/01/fifty-shades-darker-3.jpg", punishment: nil},
-{phrase: "The tea", acronym: "they're gossiping, particularly with the juiciest or most dramatic gossip.", hint: "afternoon is good time for...", img_url: "https://typeset-beta.imgix.net/elite-daily/2017/05/08030046/chip-beauty-and-the-beast.jpg", punishment: nil},
-{phrase: "GOAT", acronym: "Current usage is actually a compliment, as this is now an acronym that stands for greatest of all time.", hint: "baa", img_url: "https://www.thespruceeats.com/thmb/AWhVtBtt8qVJF9GfPuV5Az30RdM=/4494x3000/filters:fill(auto,1)/homemade-goat-cheese-with-lemon-juice-591553_15-5c40f7cbc9e77c0001f70d1d.jpg", punishment: nil},
-{phrase: "yeet", acronym: "an exclamation of excitement, approval, surprise, or all-around energy, often as issued when doing a dance move or throwing something", hint: "yahoo!", img_url: "https://media0.giphy.com/media/DvMHwFYLVHlZe/source.gif", punishment: nil}
+Last night's party was lit.", hint: "", img_url: "https://steamuserimages-a.akamaihd.net/ugc/609476610127244242/AA3757BEFCE719580024B3296AD112E0B33EEB78/", punishment: nil},
+{phrase: "Low key", acronym: "If someone or something is low key, it means it's being done under the radar or they don't want anyone to know.", hint: "", img_url: "https://static.scientificamerican.com/sciam/cache/file/00A865C6-9F1F-4412-A9CA2FC329C15F36_source.jpg?w=590&h=800&08F4EA30-9038-4EB2-8EBF9195D8829B6C", punishment: nil},
+{phrase: "Spaghetti on the Wall", acronym: "try something to see what works.", hint: "", img_url: "http://static1.squarespace.com/static/5851ea8d15d5db7317b11176/585c87a28419c2107deb3596/58b5cd171b631bbb2f832ff8/1501089566642/1ffcc2e.jpg?format=1500w", punishment: nil},
+{phrase: "Salty", acronym: "Angry or bitter about something.", hint: "", img_url: "https://pbs.twimg.com/profile_images/835940654509342724/chujJdF__400x400.jpg", punishment: nil},
+{phrase: "Savage", acronym: "Someone who roasts people nonstop and doesn't care what others will say.", hint: "", img_url: "https://iruntheinternet.com/lulzdump/images/flip-off-cat-middle-finger-goes-mad-bites-it-rage-14344850449.jpg", punishment: nil},
+{phrase: "Ship", acronym: "Short for romantic relationship, sometimes used as a verb.", hint: "", img_url: "https://i.redd.it/xsm9auf4tfq01.jpg", punishment: nil},
+{phrase: "Throw shade", acronym: "To insult or say something unkind about someone.", hint: "", img_url: "https://cdn3-www.comingsoon.net/assets/uploads/2017/01/fifty-shades-darker-3.jpg", punishment: nil},
+{phrase: "The tea", acronym: "they're gossiping, particularly with the juiciest or most dramatic gossip.", hint: "", img_url: "https://typeset-beta.imgix.net/elite-daily/2017/05/08030046/chip-beauty-and-the-beast.jpg", punishment: nil},
+{phrase: "GOAT", acronym: "Current usage is actually a compliment, as this is now an acronym that stands for greatest of all time.", hint: "", img_url: "https://media.giphy.com/media/tIeCLkB8geYtW/giphy.gif", punishment: nil},
+{phrase: "yeet", acronym: "an exclamation of excitement, approval, surprise, or all-around energy, often as issued when doing a dance move or throwing something", hint: "", img_url: "https://media0.giphy.com/media/DvMHwFYLVHlZe/source.gif", punishment: nil},
+{phrase: "Gidday", acronym: "A classic New Zealand slang word used just like hello or good day.", hint: "", img_url: "https://media.giphy.com/media/2rAKTgJIQe1buYU1R5/giphy.gif", punishment: nil},
+{phrase: "Ta", acronym: "It simply means thanks.", hint: "", img_url: "https://media.giphy.com/media/QAsBwSjx9zVKoGp9nr/giphy.gif", punishment: nil},
+{phrase: "Squizz", acronym: "This means to have a quick look in a Kiwis vocabulary.", hint: "", img_url: "https://media.giphy.com/media/Zvgb12U8GNjvq/giphy.gif", punishment: nil},
+{phrase: "Mean as", acronym: "This means sweet, great, cool. Refers to something being awesome.", hint: "", img_url: "https://media.giphy.com/media/TlK63Eu7Dwok2j8TgfS/giphy.gif", punishment: nil},
+{phrase: "Togs", acronym: "Swimming costume, swimmers or bathing costume for Kiwis.", hint: "", img_url: "https://media.giphy.com/media/etn52ENYVnpxqMaXiT/giphy.gif", punishment: nil},
+{phrase: "Gizza", acronym: "A word short for ‘give me’.", hint: "", img_url: "https://media.giphy.com/media/3oFzlWOpuCr6d1UWGY/giphy.gif", punishment: nil},
+{phrase: "Hard out", acronym: "Used when you agree with someone.", hint: "", img_url: "https://media.giphy.com/media/XT8TJ9TsxkmLm/giphy.gif", punishment: nil},
+{phrase: "Dag", acronym: "It means that something or someone is funny.", hint: "", img_url: "https://media.giphy.com/media/ff0dv4KMGxjna/giphy.gif", punishment: nil},
+{phrase: "Beached as", acronym: "Referring to the cartoon where a whale is stuck on the beach, it means when you’re stuck somewhere.", hint: "", img_url: "https://media.giphy.com/media/10fFT7qzHrN0D6/giphy.gif", punishment: nil},
+{phrase: "Pakaru", acronym: "A Maori word which is often used when something is broken.", hint: "", img_url: "https://media.giphy.com/media/5Zesu5VPNGJlm/giphy.gif", punishment: nil},
+{phrase: "yeahnah", acronym: "yes, I'm hearing what you're sayin, but No you're wrong.", hint: "", img_url: "https://www.mathsisfun.com/numbers/images/plus-minus-minus.svg", punishment: nil},
+{phrase: "Eh", acronym: "Has been referred to as an articulated question mark.", hint: "", img_url: "https://media.giphy.com/media/3oEhn4yqbvUFbcYFeU/giphy.gif", punishment: nil},
+{phrase: "Bugger all", acronym: "This is used when you have nothing left.", hint: "", img_url: "https://media3.giphy.com/media/PP3tWd7BPtWqC4IH6A/giphy.gif", punishment: nil},
+{phrase: "The wops", acronym: "A place in the middle of nowhere or far from anything.", hint: "", img_url: "https://media.giphy.com/media/8LbkGISCNEDwk/200.gif", punishment: nil},
+{phrase: "Stink one", acronym: "This means you have just disappointed someone.", hint: "", img_url: "https://thumbs.gfycat.com/InformalEnragedGalapagosdove-size_restricted.gif", punishment: nil},
+{phrase: "Skux", acronym: "This word has many meanings. The most popular is when someone looks cool or trendy.", hint: "", img_url: "https://media3.giphy.com/media/Wt6G37EJGUZPMB0Bhu/giphy.gif", punishment: nil},
+{phrase: "Sus", acronym: "When a situation or someone is practically suspicious.", hint: "", img_url: "https://media2.giphy.com/media/6bdgwW6B0ooer3r8AZ/giphy.gif", punishment: nil},
+{phrase: "Mare", acronym: "This means you’re having a difficult time.", hint: "", img_url: "https://media.giphy.com/media/SXx6ayaHyUzJVcXrX6/giphy.gif", punishment: nil},
+{phrase: "Brekkie", acronym: "Short for “breakfast”.", hint: "", img_url: "https://media.giphy.com/media/csXqmaigJhqQU/giphy.gif", punishment: nil},
+{phrase: "Lurgy", acronym: "A cold or flu.", hint: "", img_url: "https://media.giphy.com/media/s4Bi420mMDRBK/giphy.gif", punishment: nil}
 ]
 cards.each { |card| Card.create(card) }
+
 
 
 
