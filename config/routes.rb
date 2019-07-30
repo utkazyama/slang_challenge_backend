3370b5592698ab5780c367b3abf00e21496930b4
github username <github email address>
2019-07-30 18:58:56 -0500

    route bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   resources :cards
   resources :games
   resources :game_cards
-​
-  # post 'edit_card', action: :update, controller: 'cards'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
